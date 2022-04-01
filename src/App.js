1cf02fa3d8526df45f566aac1fc280f6a932512e
Knopka <knopkaivy@gmail.com>
2022-03-31 17:18:15 -0700

    Update img and generator height for horizontal phone/tablet

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import Generator from './Generator';
 function App() {
   return (
     <div className="App">
-      <div className="bgImageContainer"></div>
+      <div className="bgImageContainer">
+        {/* <img
+          src="https://images.unsplash.com/photo-1521996240847-20cdba641bdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80"
+          alt=""
+          className="img"
+        /> */}
+      </div>
       <Generator />
     </div>
   );
